9edf528f4ffe159b5d915e105f391e473866c234
Sang-Hyeon Kim <gaviator15@gmail.com>
2020-08-14 04:03:22 +0900

    Updates

---
+++ precache-manifest.4ba99f5aa98797cf2a7438db530a6694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5045bf37511cce3b3f1d3d3f65e90ea",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a7a36a86f2afa6513487",
+    "url": "/movie_app_2020/static/css/main.07ad6a09.chunk.css"
+  },
+  {
+    "revision": "da91fca76777d8e65fc5",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a36a86f2afa6513487",
+    "url": "/movie_app_2020/static/js/main.1ea1c2b9.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
